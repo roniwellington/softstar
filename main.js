bac1e7295d486b62c8c8cb5743083baf51b7a51a
Roni Wellington <roniwellington@gmail.com>
2023-03-20 18:08:10 -0300

    header em manutenção

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ $(document).ready(function () {
         $(this).addClass("active-filter").siblings().removeClass("active-filter")
     });
 });
+
+
+//////
+let profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
+}
+
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity() {
+    sideActivity.classList.toggle("open-activity")
+
+    if(sideActivity.classList.contains("open-activity")){
+
+        moreLink.innerHTML = "Show less<b>-</b>"
+
+    } else {
+        moreLink.innerHTML = "Show more<b>+</b>"
+    }
+}
